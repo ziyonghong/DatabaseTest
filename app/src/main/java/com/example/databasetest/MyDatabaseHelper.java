fe50cc1ac196823b14d62e4d4200e2ea7da6d9ff
ziyonghong <1131002466@qq.com>
2020-02-06 17:57:28 +0800

    add a table

--- app/src/main/java/com/example/databasetest/MyDatabaseHelper.java
+++ app/src/main/java/com/example/databasetest/MyDatabaseHelper.java
@@ -9,6 +9,9 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     public static final String CREATE_BOOK = "create table book ("
             + "id integer primary key autoincrement, " + "author text, "
             + "price real, " + "pages integer, " + "name text)";
+    public static final String CREATE_CATEGORY = "create table Category ("
+            + "id integer primary key autoincrement, " + "category_name text, " + "category_code integer)";
+
     private Context mContext;
     public MyDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory
             factory, int version) {
@@ -17,9 +20,13 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     }
 
     public  void onCreate(SQLiteDatabase db){
-        db.execSQL(CREATE_BOOK);  //执行建表语句
+
        Toast.makeText(mContext, "create succeeded", Toast.LENGTH_SHORT).show();
     }
 
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) { }
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(CREATE_BOOK);  //执行建表语句
+        db.execSQL(CREATE_CATEGORY);
+        onCreate(db);
+    }
 }
