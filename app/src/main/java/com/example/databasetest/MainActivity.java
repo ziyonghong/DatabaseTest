b7863c2932e4393efc5916cce62d9bf03298e895
ziyonghong <1131002466@qq.com>
2020-02-06 18:14:05 +0800

    add a table

--- app/src/main/java/com/example/databasetest/MainActivity.java
+++ app/src/main/java/com/example/databasetest/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.databasetest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +20,28 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 dbHelper.getWritableDatabase();
+            }
+        });
+
+        Button addData = (Button) findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+// 开始组装第一条数据
+                values.put("name", "The Da Vinci Code");
+                values.put("author", "Dan Brown");
+                values.put("pages", 454);
+                values.put("price", 16.96);
+                db.insert("Book", null, values); // 插入第一条数据
+                values.clear();
+// 开始组装第二条数据
+                values.put("name", "The Lost Symbol");
+                values.put("author", "Dan Brown");
+                values.put("pages", 510);
+                values.put("price", 19.95);
+                db.insert("Book", null, values); // 插入第二条数据
             }
         });
     }
